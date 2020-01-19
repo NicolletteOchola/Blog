c4800cb2961c0f8c3b4c8e1ba1f8b1010bf67ded
Nicole Ocholla <nicoleochola@gmail.com>
2020-01-19 20:31:40 +0300

    Added all imports to run.py for the app to run

--- run.py
+++ run.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from app import create_app, db
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Server
+from app.models import *
 
-@app.route("/")
-@app.route('/home')
-def home():
-    return render_template('home.html')
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
+# app = create_app('production')
+app = create_app('development')
 
+manager = Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('runserver', Server)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User= User)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
