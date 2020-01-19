4f1410e9a9cf9bcb00727f47443b1021255279e7
Nicole Ocholla <nicoleochola@gmail.com>
2020-01-19 21:05:53 +0300

    Added functions to delete a post

--- app/posts/views.py
+++ app/posts/views.py
@@ -84,4 +84,13 @@ def update_post(post_id):
     myposts = Post.query.order_by(Post.posted_date.desc())
     return render_template('new-post.html', title='Update Post', form=form, legend='Update Post', quotes=quotes, myposts=myposts)
 
-
+@posts.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('main.home'))
